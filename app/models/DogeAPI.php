33c59198956c8188e7df7cf41487e186e2f974c9
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-01-07 20:31:58 -0500

    Updated str

--- app/models/DogeAPI.php
+++ app/models/DogeAPI.php
@@ -16,7 +16,7 @@ class DogeAPI {
 
   public static function getBalance($address) {
     $str = "https://www.dogeapi.com/wow/?api_key=" . Config::get('app.doge_api_key');
-    $str .= "&a=get_address_recieved&payment_address=" . $address;
+    $str .= "&a=get_address_received&payment_address=" . $address;
     return json_decode(file_get_contents($str));
   }
 }
