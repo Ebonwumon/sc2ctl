57fee28fbfc94668c00e88f26cb9c0b50e9b673c
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-12-11 20:34:08 -0700

    added example env

---
+++ .example.env.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'MANDRILL_SECRET' => '',
+    'BNET_CLIENT_ID' => "",
+    'BNET_CLIENT_SECRET' => "",
+    'BNET_REDIRECT_URI' => "",
+
+    'DATABASE_DATABASE' => "",
+    'DATABASE_USER' => "",
+    'DATABASE_PASSWORD' => "",
+];
